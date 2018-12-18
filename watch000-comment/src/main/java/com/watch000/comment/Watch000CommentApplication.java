42009331c7689071cc00b1b6a259e93860a5e607
isaac <jij444@hotmail.com>
2018-12-18 16:04:58 +0800

    add comment project

---
+++ watch000-comment/src/main/java/com/watch000/comment/Watch000CommentApplication.java
@@ -0,0 +1,14 @@
+package com.watch000.comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Watch000CommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Watch000CommentApplication.class, args);
+	}
+
+}
+
