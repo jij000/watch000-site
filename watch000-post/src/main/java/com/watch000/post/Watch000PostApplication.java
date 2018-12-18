7c812f7f6b8797836f73662ac2cde96bf4975c3a
isaac <jij444@hotmail.com>
2018-12-18 16:01:26 +0800

    add post project

---
+++ watch000-post/src/main/java/com/watch000/post/Watch000PostApplication.java
@@ -0,0 +1,14 @@
+package com.watch000.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Watch000PostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Watch000PostApplication.class, args);
+	}
+
+}
+
---
+++ watch000-post/src/test/java/com/watch000/post/Watch000PostApplicationTests.java
@@ -0,0 +1,17 @@
+package com.watch000.post;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Watch000PostApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
